732ce78232d04e7d2620118ad5ebabfc404fa880
zhaobingbing.ices <zhaobingbing.ices@bytedance.com>
2020-06-26 22:40:39 +0800

    增加接口文件

--- consts/school_name.go
+++ consts/school_name.go
@@ -2,4 +2,5 @@ package consts
 
 const (
 	BOZHOU_UNIVERSITY = "亳州学院"
+	HRBUST_UNIVERSITY = "哈尔滨理工大学"
 )
--- verification_code/verification_code_loder.go
+++ verification_code/verification_code_loder.go
@@ -4,6 +4,7 @@ import "my_project/consts"
 
 var verifyCodeLink = map[string]string{
 	consts.BOZHOU_UNIVERSITY:"http://211.141.201.154/CheckCode.aspx",
+	consts.HRBUST_UNIVERSITY: "http://jwzx.hrbust.edu.cn/academic/getCaptcha.do",
 }
 
 func GetVerifyCodeLinkBySchoolName(schoolName string)string{
